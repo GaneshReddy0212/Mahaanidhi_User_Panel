99f9c99c43cdc422f2859728eb0e3f8ce6fd615c
Ganesh <kganesh181888@gmail.com>
2024-07-18 10:57:13 +0530

    prod url changes added ts file

---
+++ src/app/Environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://ganeshreddy0212-001-site1.ctempurl.com/api/',
+    imageUrl: 'https://ganeshreddy0212-001-site1.ctempurl.com/uploads/' // Production URL for images
+  };
--- src/app/Environment/environment.service.ts
+++ src/app/Environment/environment.service.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     apiUrl: 'https://ganeshreddy0212-001-site1.ctempurl.com/api/',
     ImageUrl: 'https://ganeshreddy0212-001-site1.ctempurl.com/uploads/',
     // other service-specific configurations
