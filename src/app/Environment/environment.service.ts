e470b765f94a1c749054629c4f8ab78978d360b4
Ganesh <kganesh181888@gmail.com>
2024-07-18 10:51:47 +0530

    prod url changes

--- src/app/Environment/environment.service.ts
+++ src/app/Environment/environment.service.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     apiUrl: 'https://ganeshreddy0212-001-site1.ctempurl.com/api/',
     ImageUrl: 'https://ganeshreddy0212-001-site1.ctempurl.com/uploads/',
     // other service-specific configurations
